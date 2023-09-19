64fd4fab7517f5bc9ab45f40cf569ebf5766f387
Amr006 <2021170374@cis.asu.edu.eg>
2023-09-20 00:29:48 +0300

    setting up server

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      //decodes token id
+      const decode = jwt.verify(token, process.env.SECRET_KEY);
+
+      req.userId = decode.Id;
+      req.userName = decode.Name;
+      
+      return next();
+    } catch (error) {
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
+
+module.exports = protect;
