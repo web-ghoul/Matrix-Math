94b1b1eef926e27928b8db7f5efc4c96ca81ed44
webGhoul <84246173+web-ghoul@users.noreply.github.com>
2023-09-20 19:07:13 +0300

    first commit for client

---
+++ client/jest.config.mjs
@@ -0,0 +1,17 @@
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+/** @type {import('jest').Config} */
+const config = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "^@/components/(.*)$": "<rootDir>/components/$1",
+    "^@/pages/(.*)$": "<rootDir>/pages/$1",
+  },
+  testEnvironment: "jest-environment-jsdom",
+};
+
+export default createJestConfig(config);
---
+++ client/src/app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Matrix Math",
+  description: "App For Manage Students",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
